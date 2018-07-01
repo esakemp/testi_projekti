ed2878caee662b8e195f6bfcb3b777d7e76a305e
Esa Kemppainen <esa.kemppainen@helsinki.fi>
2018-07-01 15:05:41 +0300

    bug fixes

--- application/__init__.py
+++ application/__init__.py
@@ -1,17 +1,18 @@
+#flask init
 from flask import Flask
 app = Flask(__name__)
 
+#database init
 from flask_sqlalchemy import SQLAlchemy
-
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///artists.db"
-
 app.config["SQLALCHEMY_ECHO"] = True
 
 db = SQLAlchemy(app)
 
+#files from application folder
 from application import views
 
-#files from albums folder
+#files from artists folder
 from application.artists import models
 from application.artists import views
 
@@ -19,4 +20,21 @@ from application.artists import views
 from application.auth import models
 from application.auth import views
 
+#login functionality
+from application.auth.models import User
+from os import urandom
+app.config["SECRET_KEY"] = urandom(32)
+
+from flask_login import LoginManager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+login_manager.login_view = "auth_login"
+login_manager.login_message = "Please login to use this functionality."
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
+#create database if needed
 db.create_all()
