35edfae82018bea28bddc457e93232005cec0791
Esa Kemppainen <esa.kemppainen@helsinki.fi>
2018-07-01 12:53:09 +0300

    linkit listaukseen ja albumin lisäykseen, bugi korjauksia, voi vaihtaa omistaako levyn vai ei

--- application/albums/views.py
+++ application/albums/views.py
@@ -12,10 +12,37 @@ def albums_index():
 def albums_form():
     return render_template("albums/new.html")
 
+#mark album as owned or not owned
+@app.route("/albums/<album_id>/", methods=["POST"])
+def albums_set_owned(album_id):
+    
+    a = Album.query.get(album_id)
+
+    if a.owned == True:
+        a.owned = False
+    else:
+        a.owned = True    
+
+    #a.owned = True
+    db.session().commit()   
+
+    return redirect(url_for("albums_index")) 
+
+#mark album as not owned
+@app.route("/albums/<album_id>/", methods=["POST"])
+def albums_set_not_owned(album_id):
+
+    a = Album.query.get(album_id)
+    a.owned = False
+    db.Session().commit()
+
+    return redirect(url_for("albums_index"))
+
+
 #creates new album
 @app.route("/albums/", methods=["POST"])
 def albums_create():
-    t = Task(request.form.get("name"))
+    t = Album(request.form.get("name"))
 
     db.session().add(t)
     db.session().commit()
