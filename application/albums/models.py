2dc96d631a96825ea569a635bc5a8cd901637819
Esa Kemppainen <esa.kemppainen@helsinki.fi>
2018-07-01 12:03:13 +0300

    bug fixes

--- application/albums/models.py
+++ application/albums/models.py
@@ -2,8 +2,8 @@ from application import db
 
 class Album(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    date_created = db.Column(db.Datetime, default=db.func.current_timestamp())
-    date_modified = db.column(db.Datetime, default=db.func.current_timestamp(), 
+    date_created = db.Column(db.DateTime, default=db.func.current_timestamp())
+    date_modified = db.Column(db.DateTime, default=db.func.current_timestamp(),
     onupdate=db.func.current_timestamp())
 
     name = db.Column(db.String(144), nullable=False)
--- application/albums/views.py
+++ application/albums/views.py
@@ -1,10 +1,18 @@
 from application import app, db
 from flask import redirect, render_template, request, url_for
+from application.albums.models import Album
 
+#list all albums
+@app.route("/albums", methods=["GET"])
+def album_index():
+    return render_template("albums/list.html", albums = Album.query.all())
+
+#shows form for adding albums
 @app.route("/albums/new")
 def albums_form():
-    return render_template(albums/new.html)
+    return render_template("albums/new.html")
 
+#creates new album
 @app.route("/albums/", methods=["POST"])
 def albums_create():
     t = Task(request.form.get("name"))
